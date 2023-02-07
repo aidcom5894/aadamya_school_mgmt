6c4e549235c0e237007fd9a9312242831f54fc41
aidcom5894 <vivekrobinkujur_jsr@hotmail.com>
2023-02-07 14:06:27 +0530

    Updated Master Page

---
+++ master_content/admin_master/admin_footer.php
@@ -0,0 +1,38 @@
+
+
+<!-- Footer Area Start Here -->
+<footer class="footer-wrap-layout1">
+<div class="copyright">© Copyrights <a href="#">akkhor</a> 2019. All rights reserved. Designed by <a
+href="#">PsdBosS</a></div>
+</footer>
+<!-- Footer Area End Here -->
+</div>
+</div>
+<!-- Page Area End Here -->
+</div>
+<!-- jquery-->
+<script src="../../modules/dashboard/js/jquery-3.3.1.min.js"></script>
+<!-- Plugins js -->
+<script src="../../modules/dashboard/js/plugins.js"></script>
+<!-- Popper js -->
+<script src="../../modules/dashboard/js/popper.min.js"></script>
+<!-- Bootstrap js -->
+<script src="../../modules/dashboard/js/bootstrap.min.js"></script>
+<!-- Counterup Js -->
+<script src="../../modules/dashboard/js/jquery.counterup.min.js"></script>
+<!-- Moment Js -->
+<script src="../../modules/dashboard/js/moment.min.js"></script>
+<!-- Waypoints Js -->
+<script src="../../modules/dashboard/js/jquery.waypoints.min.js"></script>
+<!-- Scroll Up Js -->
+<script src="../../modules/dashboard/js/jquery.scrollUp.min.js"></script>
+<!-- Full Calender Js -->
+<script src="../../modules/dashboard/js/fullcalendar.min.js"></script>
+<!-- Chart Js -->
+<script src="../../modules/dashboard/js/Chart.min.js"></script>
+<!-- Custom Js -->
+<script src="../../modules/dashboard/js/main.js"></script>
+
+</body>
+
+</html>
---
+++ master_content/admin_master/admin_header.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html class="no-js" lang="">
+
+<head>
+<meta charset="utf-8">
+<meta http-equiv="x-ua-compatible" content="ie=edge">
+<title>AKKHOR | Home 1</title>
+<meta name="description" content="">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<!-- Favicon -->
+<link rel="shortcut icon" type="image/x-icon" href="../../modules/dashboard/img/favicon.png">
+<!-- Normalize CSS -->
+<link rel="stylesheet" href="../../modules/dashboard/css/normalize.css">
+<!-- Main CSS -->
+<link rel="stylesheet" href="../../modules/dashboard/css/main.css">
+<!-- Bootstrap CSS -->
+<link rel="stylesheet" href="../../modules/dashboard/css/bootstrap.min.css">
+<!-- Fontawesome CSS -->
+<link rel="stylesheet" href="../../modules/dashboard/css/all.min.css">
+<!-- Flaticon CSS -->
+<link rel="stylesheet" href="../../modules/dashboard/fonts/flaticon.css">
+<!-- Full Calender CSS -->
+<link rel="stylesheet" href="../../modules/dashboard/css/fullcalendar.min.css">
+<!-- Animate CSS -->
+<link rel="stylesheet" href="../../modules/dashboard/css/animate.min.css">
+<!-- Custom CSS -->
+<link rel="stylesheet" href="../../modules/dashboard/css/style.css">
+<!-- Modernize js -->
+<script src="../../modules/dashboard/js/modernizr-3.6.0.min.js"></script>
+</head>
